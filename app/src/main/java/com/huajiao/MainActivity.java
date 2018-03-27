182a23800870ad955a4fafa061e665b9dfc244d6
yutianzuo <yutianzuo@meituan.com>
2018-03-27 10:00:04 +0800

    new proj

---
+++ app/src/main/java/com/huajiao/MainActivity.java
@@ -0,0 +1,16 @@
+package com.huajiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+}
